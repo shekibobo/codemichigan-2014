9f75ce1f2f7e7cdb29ae04403ed9dd5056f9fb64
palegin <dillonbbailly@gmail.com>
2014-10-05 03:38:26 -0400

    This Water Activity leaves much to be desired

--- app/src/main/java/com/codebits/codemichigan/michiganoutdoors/activities/WaterAttrationActivity.java
+++ app/src/main/java/com/codebits/codemichigan/michiganoutdoors/activities/WaterAttrationActivity.java
@@ -4,14 +4,24 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import com.codebits.codemichigan.michiganoutdoors.R;
+import com.codebits.codemichigan.michiganoutdoors.data.models.StateWaterAttraction;
 
 public class WaterAttrationActivity extends Activity {
+
+    private ListView lView;
+    private StateWaterAttraction sWaterAttraction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_water_attration);
+        lView = (ListView) findViewById(R.id.water_list_view);
+        //final Adapter adapter = new ArrayAdapter<String>(this,R.layout.michigan_attraction_list_item,sWaterAttraction);
     }
 
 
