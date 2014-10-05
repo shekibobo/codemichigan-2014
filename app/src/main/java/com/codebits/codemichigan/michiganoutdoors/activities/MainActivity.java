9466d845247e1ff6e5e291e7e7261a09857103ae
White0ut <kdecline@gmail.com>
2014-10-05 03:40:56 -0400

    naive reactivelocation implementation

--- app/src/main/java/com/codebits/codemichigan/michiganoutdoors/activities/MainActivity.java
+++ app/src/main/java/com/codebits/codemichigan/michiganoutdoors/activities/MainActivity.java
@@ -2,10 +2,12 @@ package com.codebits.codemichigan.michiganoutdoors.activities;
 
 import android.app.ActionBar;
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.widget.DrawerLayout;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.SearchView;
@@ -27,12 +29,16 @@ import com.codebits.codemichigan.michiganoutdoors.fragments.FilterDrawerFragment
 import java.util.ArrayList;
 import java.util.List;
 
+import pl.charmas.android.reactivelocation.ReactiveLocationProvider;
 import rx.Observable;
+import rx.Subscription;
 import rx.android.observables.AndroidObservable;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 import com.codebits.codemichigan.michiganoutdoors.fragments.AttractionsFragment;
+import com.google.android.gms.location.LocationRequest;
 
 import org.parceler.Parcels;
 
@@ -43,6 +49,11 @@ public class MainActivity extends FragmentActivity
     ArrayList<MichiganAttraction> resourceArray;
     private FilterDrawerFragment mFilterDrawerFragment;
     private ActionBar actionBar;
+
+    private ReactiveLocationProvider locationProvider;
+
+    private Observable<Location> lastKnownLocationObservable;
+    private Observable<Location> locationUpdatesObservable;
 
     private AttractionsFragment mAttractionsFragment;
     private DrawerLayout mDrawerLayout;
@@ -76,16 +87,38 @@ public class MainActivity extends FragmentActivity
                 R.id.filter_drawer,
                 mDrawerLayout);
 
+        locationProvider = new ReactiveLocationProvider(getApplicationContext());
+        lastKnownLocationObservable = locationProvider.getLastKnownLocation();
+
+        locationUpdatesObservable = locationProvider.getUpdatedLocation(
+                LocationRequest.create()
+                        .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                        .setNumUpdates(5)
+                        .setInterval(100)
+        );
 
         //reloadResourcesFromFilters(null);
     }
 
     private void reloadResourcesFromFilters(String query) {
         resourceArray.clear();
-        AndroidObservable.bindActivity(this, Observable.merge(landAttractionRequest(query), waterAttractionRequest(query)))
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(s -> updateDataSet(s));
+        Log.wtf("KENNY","Trying");
+        if (mFilterDrawerFragment.isChecked(FilterDrawerFragment.FIND_ME_FILTER_INDEX)) {
+            Log.wtf("KENNY", "Registering Observable");
+            AndroidObservable.bindActivity(this, locationUpdatesObservable)
+                    .subscribeOn(Schedulers.newThread())
+                    .map(new LocationToQueryFunc())
+                    .flatMap(location -> Observable.merge(landAttractionRequest(query, location), waterAttractionRequest(query, location)))
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(s -> updateDataSet(s));
+        } else {
+            //////     Regular Option
+            AndroidObservable.bindActivity(this, Observable.merge(landAttractionRequest(query, null), waterAttractionRequest(query, null)))
+                    .subscribeOn(Schedulers.newThread())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(s -> updateDataSet(s));
+        }
+
         if (landNotFound && waterNotFound) {
             // Clear all items
             updateHeaderView();
@@ -97,7 +130,7 @@ public class MainActivity extends FragmentActivity
         }
     }
 
-    private Observable<List<StateWaterAttraction>> waterAttractionRequest(String query) {
+    private Observable<List<StateWaterAttraction>> waterAttractionRequest(String query, String location) {
         ArrayList<String> queriesForWater = new ArrayList<>(2);
         if (mFilterDrawerFragment.isChecked(FilterDrawerFragment.LAKE_FILTER_INDEX))
             queriesForWater.add(LakeAttraction.toQuery());
@@ -109,12 +142,18 @@ public class MainActivity extends FragmentActivity
             waterNotFound = true;
             return Observable.empty();
         } else {
+            // if location != null
             String waterQuery = TextUtils.join(" OR ", queriesForWater);
+            waterQuery = "(" + waterQuery + ")";
+            if (location != null) {
+                waterQuery = location + waterQuery;
+                Log.wtf("KENNY", waterQuery);
+            }
             return dataService.stateWaterAttractionList(waterQuery, query);
         }
     }
 
-    private Observable<List<StateLandAttraction>> landAttractionRequest(String query) {
+    private Observable<List<StateLandAttraction>> landAttractionRequest(String query, String location) {
         ArrayList<String> queriesForLand = new ArrayList<>(4);
         if (mFilterDrawerFragment.isChecked(FilterDrawerFragment.CAMPGROUND_FILTER_INDEX))
             queriesForLand.add(StateForestCampground.toQuery());
@@ -132,7 +171,13 @@ public class MainActivity extends FragmentActivity
             landNotFound = true;
             return Observable.empty();
         } else {
+            // if location != null
             String landQuery = TextUtils.join(" OR ", queriesForLand);
+            landQuery = "(" + landQuery + ")";
+            if (location != null) {
+                landQuery = location + landQuery;
+                Log.wtf("KENNY", landQuery);
+            }
             return dataService.stateLandAttractionList(landQuery, query);
         }
     }
@@ -247,4 +292,15 @@ public class MainActivity extends FragmentActivity
             startActivity(intent);
         }
     }
+
+    private static class LocationToQueryFunc implements Func1<Location, String> {
+        @Override
+        public String call(Location location) {
+            // within_circle(location_col_identifier, latitude, longitude, radius)
+            String res = "within_circle(location, " + location.getLatitude() + ", " + location.getLongitude() + ", 32186) AND ";
+            Log.wtf("KENNY","Coverted loc to String\n=> " + res);
+            return res;
+        }
+    }
+
 }
