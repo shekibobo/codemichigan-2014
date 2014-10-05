9c9f45bba2b3df81fc6b71c69624d35a1437724f
Joshua Kovach <kovach.jc@gmail.com>
2014-10-05 05:08:26 -0500

    Use OKHttp for requests

--- app/src/main/java/com/codebits/codemichigan/michiganoutdoors/data/api/services/MichiganData.java
+++ app/src/main/java/com/codebits/codemichigan/michiganoutdoors/data/api/services/MichiganData.java
@@ -1,7 +1,10 @@
 package com.codebits.codemichigan.michiganoutdoors.data.api.services;
 
+import com.squareup.okhttp.OkHttpClient;
+
 import lombok.Getter;
 import retrofit.RestAdapter;
+import retrofit.client.OkClient;
 import retrofit.converter.GsonConverter;
 
 /**
@@ -13,10 +16,13 @@ public class MichiganData {
     @Getter MichiganDataService dataService;
 
     public MichiganData() {
+        OkHttpClient okHttpClient = new OkHttpClient();
+
         restAdapter = new RestAdapter.Builder()
                 .setEndpoint("http://data.michigan.gov")
                 .setRequestInterceptor(new MichiganDataRequestIntercepter())
                 .setConverter(new GsonConverter(MichiganDataGson.getGson()))
+                .setClient(new OkClient(okHttpClient))
                 .build();
 
         dataService = restAdapter.create(MichiganDataService.class);
