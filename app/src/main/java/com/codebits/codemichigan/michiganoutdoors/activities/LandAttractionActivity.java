258396ae3de838944ece107d7da5d6faaa8060b7
Joshua Kovach <kovach.jc@gmail.com>
2014-10-05 02:23:31 -0500

    Style the land activity and fill in data

--- app/src/main/java/com/codebits/codemichigan/michiganoutdoors/activities/LandAttractionActivity.java
+++ app/src/main/java/com/codebits/codemichigan/michiganoutdoors/activities/LandAttractionActivity.java
@@ -2,16 +2,33 @@ package com.codebits.codemichigan.michiganoutdoors.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
 import com.codebits.codemichigan.michiganoutdoors.R;
 import com.codebits.codemichigan.michiganoutdoors.data.models.StateLandAttraction;
 
 import org.parceler.Parcels;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 public class LandAttractionActivity extends Activity {
 
     StateLandAttraction mLandAttraction;
+    @InjectView(R.id.site_name_heading) TextView siteNameHeading;
+    @InjectView(R.id.site_county_text) TextView siteCountyAndCampingTypes;
+    @InjectView(R.id.site_call_button) Button siteCallButton;
+    @InjectView(R.id.site_permit_text) TextView sitePermitText;
+    @InjectView(R.id.site_locate_button) Button siteLocationButton;
+    @InjectView(R.id.campground_detailed_text) TextView detailText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +36,26 @@ public class LandAttractionActivity extends Activity {
         setContentView(R.layout.activity_land_attraction);
 
         mLandAttraction = Parcels.unwrap(this.getIntent().getExtras().getParcelable("STATE_LAND_ATTRACTION"));
+
+        ButterKnife.inject(this);
+
+        siteNameHeading.setText(mLandAttraction.getName());
+
+        List<String> countyAndStyle = Arrays.asList(mLandAttraction.getCounty());
+        if (mLandAttraction.getCampingStyles() != null) countyAndStyle.add(mLandAttraction.getCampingStyles());
+        siteCountyAndCampingTypes.setText(TextUtils.join(" | ", countyAndStyle));
+
+        siteCallButton.setText(mLandAttraction.getPhone());
+        siteLocationButton.setText(R.string.locate);
+        sitePermitText.setText("Permit Required: " + (mLandAttraction.isPermitRequired() ? "Yes" : "No"));
+
+        List<String> otherStuff = new ArrayList<>();
+        if (mLandAttraction.getDescription() != null) otherStuff.add(mLandAttraction.getDescription());
+        if (mLandAttraction.getGroupRate() > 0) otherStuff.add("Group Rate: " + mLandAttraction.getGroupRate());
+        if (mLandAttraction.getTotalNumOfSites() > 0) otherStuff.add("Total Number of Sites: " + mLandAttraction.getTotalNumOfSites());
+        if (mLandAttraction.getVehicles() != null) otherStuff.add("Allowed Vehicles: " + mLandAttraction.getVehicles());
+
+        detailText.setText(TextUtils.join("\n\n", otherStuff));
     }
 
 
